aa5df3750e238e4d3529ec461f6eb3b8d93e1b42
MichellMurallas <michellsms893@gmail.com>
2022-12-22 14:22:45 -0500

    Dependencies

---
+++ Api-rest-red-social/index.js
@@ -0,0 +1,11 @@
+//Conexion a base de datos:
+
+// Crear servidor Node:
+
+//Configurar Cors:
+
+//Convertir los datos del body a objetos js:
+
+// Cargar conf rutas:
+
+//poner servidor a escuchar http:
