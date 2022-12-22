4beb1c3cc66a89c8b0343229d47cb0fa2409f7f6
MichellMurallas <michellsms893@gmail.com>
2022-12-22 15:20:13 -0500

    connecting to the database

--- Arrancar-proyecto.bat
+++
@@ -1,2 +0,0 @@
-start cmd.exe /k "cd C:\Program Files\MongoDB\Server\6.0\bin && mongod.exe --dbpath C:\data\db"
-start cmd.exe /k "cd C:\Users\migo0\Desktop\Codigo\BACKEN\Api-SocialRed && npm start"
